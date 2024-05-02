6ca873fdb35610883e5afd0e6f5648187296a4e9
douglasroberto90 <douglas_roberto90@hotmail.com>
2024-05-02 18:10:27 -0300

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -31,7 +31,7 @@ class _HomePageState extends State<HomePage> {
                   } else {
                     return Text(
                       '${snapshot.data}',
-                      style: Theme.of(context).textTheme.headline4,
+                      style: Theme.of(context).textTheme.headlineMedium,
                     );
                   }
                 }),
@@ -40,7 +40,7 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: bloc.incrementar,
-        tooltip: 'Increment',
+        tooltip: 'Incrementar',
         child: const Icon(Icons.add),
       ),
     );
